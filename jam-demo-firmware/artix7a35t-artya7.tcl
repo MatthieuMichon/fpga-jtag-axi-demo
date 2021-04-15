7e386a272c2995212625ee9f125f90d418ffd32d
Matthieu Michon <matthieu.michon@gmail.com>
2021-04-15 23:30:23 +0200

    Add IP Integrator support

--- jam-demo-firmware/artix7a35t-artya7.tcl
+++ jam-demo-firmware/artix7a35t-artya7.tcl
@@ -1,3 +1,52 @@
+proc create_bd {} {
+	create_project \
+		-part xc7a35t-csg324-1 \
+		-verbose \
+		bd_project \
+		vivado_bd_project_dir
+
+	create_bd_design bd_design_1
+
+	set clock [create_bd_port -dir I -type clk -freq_hz 100000000 clock]
+	set resetn [create_bd_port -dir I -type rst resetn]
+
+	set jtag_axi_0 [create_bd_cell \
+		-type ip \
+		-vlnv xilinx.com:ip:jtag_axi:1.2 \
+		jtag_axi_0 \
+	]
+
+	set axi_bram_ctrl_0 [create_bd_cell \
+		-type ip \
+		-vlnv xilinx.com:ip:axi_bram_ctrl:4.1 \
+		axi_bram_ctrl_0 \
+	]
+
+	set axi_bram_ctrl_0_bram [create_bd_cell \
+		-type ip \
+		-vlnv xilinx.com:ip:blk_mem_gen:8.4 \
+		axi_bram_ctrl_0_bram \
+	]
+
+	set_property -dict [list CONFIG.Memory_Type True_Dual_Port_RAM] $axi_bram_ctrl_0_bram
+
+	connect_bd_intf_net -intf_net axi_bram_ctrl_0_BRAM_PORTA [get_bd_intf_pins axi_bram_ctrl_0/BRAM_PORTA] [get_bd_intf_pins axi_bram_ctrl_0_bram/BRAM_PORTA]
+	connect_bd_intf_net -intf_net axi_bram_ctrl_0_BRAM_PORTB [get_bd_intf_pins axi_bram_ctrl_0/BRAM_PORTB] [get_bd_intf_pins axi_bram_ctrl_0_bram/BRAM_PORTB]
+	connect_bd_intf_net -intf_net jtag_axi_0_M_AXI [get_bd_intf_pins axi_bram_ctrl_0/S_AXI] [get_bd_intf_pins jtag_axi_0/M_AXI]
+																														
+	# Create port connections                                                                                            
+	connect_bd_net -net clock [get_bd_ports clock] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk] [get_bd_pins jtag_axi_0/aclk]
+	connect_bd_net -net resetn [get_bd_ports resetn] [get_bd_pins axi_bram_ctrl_0/s_axi_aresetn] [get_bd_pins jtag_axi_0/aresetn]
+																														
+	# Create address segments                                                                                            
+	assign_bd_address -offset 0xC0000000 -range 0x00002000 -target_address_space [get_bd_addr_spaces jtag_axi_0/Data] [get_bd_addr_segs axi_bram_ctrl_0/S_AXI/Mem0] -force
+																														
+	validate_bd_design
+	save_bd_design
+
+	generate_target synthesis [get_files ./jam-demo-firmware/vivado_bd_project_dir/bd_project.srcs/sources_1/bd/bd_design_1/bd_design_1.bd]
+}
+
 proc synthesize {} {
 	foreach sv_file [glob *.sv] {
 		read_verilog -sv ${sv_file}
@@ -48,6 +97,7 @@ proc reconfigure {} {
 
 set action [lindex $argv 0]
 switch ${action} {
+	"create_bd" { create_bd }
 	"synthesize" { synthesize }
 	"reconfigure" { reconfigure }
 }
